208f75b57160add49ee973b07112aef2a3d0a4bf
Roy Rebuffo <roy.rebuffo@bootcamp-upgrade.com>
2025-09-25 20:50:24 +0200

    add ejercicio matrices bidimensionales

--- src/primero/MatricesEstaticas.java
+++ src/primero/MatricesEstaticas.java
@@ -25,15 +25,68 @@ public class MatricesEstaticas {
 	public static void visualizar() {
 		for (int i = 0; i < tb.length; i++) {
 			for (int j = 0; j < tb[i].length; j++) {
-				System.out.print("   " +tb[i][j]);//Espacio entre filas y cols
+				System.out.print("   " +tb[i][j]);//Espacio "  " +... entre filas y cols
 			}
 			System.out.println();//Salto de linea
 		}
 	}
+	
+	public static void sumarFilas() {
+	    for (int i = 0; i < tb.length; i++) {
+	        int sumaFila = 0; // acumulador POR FILA
+	        for (int j = 0; j < tb[i].length; j++) {
+	            sumaFila += tb[i][j]; 
+	        }
+	        System.out.println("Suma de la fila " + i + ": " + sumaFila);
+	    }
+	}
+
+	
+	public static void sumarCols() {
+	    for (int j = 0; j < tb[0].length; j++) {
+	    	int cols = 0; // acumulador por columna
+	        for (int i = 0; i < tb.length; i++) {
+	        	cols += tb[i][j];
+	        }
+	        System.out.println("Suma de la columna " + j + ": " + cols);
+	    }
+	}
+
+	public static void media() {
+	    double mediaRows =0;
+	    double mediaCols = 0;
+	    // recorrer filas
+	    for (int i = 0; i < tb.length; i++) {
+	        int t_rows = 0;
+	        int c_rows = 0;
+	        for (int j = 0; j < tb[i].length; j++) {
+	            t_rows += tb[i][j];
+	            c_rows++;
+	        }
+	        mediaRows = (double) t_rows / c_rows;
+	        System.out.printf("Media fila %d: %.2f\n", i, mediaRows);
+	    }
+
+	    // recorrer columnas
+	    for (int j = 0; j < tb[0].length; j++) {
+	    		int t_cols = 0;
+	    		int c_cols = 0;
+	        for (int i = 0; i < tb.length; i++) {
+	            t_cols += tb[i][j];
+	            c_cols++;
+	        }
+	        mediaCols = (double) t_cols / c_cols;
+	        System.out.printf("Media columna es: %.2f\n", mediaCols);
+	    }
+	}
+
+	
 	public static void main(String[] args) {
 		rellenarTB();
 		visualizar();
-		
+		sumarFilas();
+		sumarCols();
+		media();
 	}
 
 }
