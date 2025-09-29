0736919a63d5409f9171310203702243113dc2bc
Roy Rebuffo <roy.rebuffo@bootcamp-upgrade.com>
2025-09-29 19:56:52 +0200

    ft ejercicio 1 propuesto jose

--- src/primero/MatricesEstaticas.java
+++ src/primero/MatricesEstaticas.java
@@ -4,6 +4,7 @@
 //rellenarlas con numeros aleatorios de 0 a 10
 //imprimir la matriz en la que esten inculidas de los valores por fila y la suma de los valores por col.
 //calcular medias por filas y columnas
+
 package primero;
 
 public class MatricesEstaticas {
@@ -33,18 +34,17 @@ public class MatricesEstaticas {
 	
 	public static void sumarFilas() {
 	    for (int i = 0; i < tb.length; i++) {
-	        int sumaFila = 0; // acumulador POR FILA
+	        int sumaFila = 0; // acumulador POR FILA, iniciamos dentro del bucle para que no acumule las filas y se reinicie por "vuelta"
 	        for (int j = 0; j < tb[i].length; j++) {
 	            sumaFila += tb[i][j]; 
 	        }
 	        System.out.println("Suma de la fila " + i + ": " + sumaFila);
 	    }
 	}
-
 	
 	public static void sumarCols() {
 	    for (int j = 0; j < tb[0].length; j++) {
-	    	int cols = 0; // acumulador por columna
+	    	int cols = 0; // acumulador por columna, iniciamos dentro del bucle para que no acumule las cols y se reinicie por "vuelta"
 	        for (int i = 0; i < tb.length; i++) {
 	        	cols += tb[i][j];
 	        }
@@ -56,7 +56,7 @@ public class MatricesEstaticas {
 	    double mediaRows =0;
 	    double mediaCols = 0;
 	    // recorrer filas
-	    for (int i = 0; i < tb.length; i++) {
+	    for (int i = 0; i < tb.length; i++) {//iniciamos dentro del bucle para que no acumule las filas y se reinicie por "vuelta"
 	        int t_rows = 0;
 	        int c_rows = 0;
 	        for (int j = 0; j < tb[i].length; j++) {
@@ -68,7 +68,7 @@ public class MatricesEstaticas {
 	    }
 
 	    // recorrer columnas
-	    for (int j = 0; j < tb[0].length; j++) {
+	    for (int j = 0; j < tb[0].length; j++) {//iniciamos dentro del bucle para que no acumule las filas y se reinicie por "vuelta"
 	    		int t_cols = 0;
 	    		int c_cols = 0;
 	        for (int i = 0; i < tb.length; i++) {
@@ -79,7 +79,6 @@ public class MatricesEstaticas {
 	        System.out.printf("Media columna es: %.2f\n", mediaCols);
 	    }
 	}
-
 	
 	public static void main(String[] args) {
 		rellenarTB();
@@ -88,5 +87,4 @@ public class MatricesEstaticas {
 		sumarCols();
 		media();
 	}
-
 }
